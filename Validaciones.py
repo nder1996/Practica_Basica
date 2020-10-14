896c7699be56871f2ecc283db83060d15c46efd8
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2020-10-14 18:43:22 -0500

    Beta_Aburrido

--- Validaciones.py
+++ Validaciones.py
@@ -6,28 +6,42 @@ import sys
 
 
 
-V1 = "" ; V2 = "" ; k=0 ; linea=""
-V_T=[] ; Votantes=[]
+V1 = "" ; V2 = "" ; k=0 ; linea="" ; i=0 ; j=0
+Votante=[] ; Com_Votantes=[]
 
 Data = ""
 
 
 
 V1 = open("Prueba.txt",'r')
+
 for linea in V1.read():   
    if linea!="-" or linea!="\n" or linea!=" ":
       if (re.search( "[A-Za-z]",linea)!=None) or (linea.isnumeric()==True):
            Data = Data + linea
    if linea==" ":
           if Data!="":
-             Votantes.append(Data)
+             Com_Votantes.append(Data)
              Data = ""
+   if linea=="*":
+      Com_Votantes.append("*")
 
-           
+V1.close()
+
+os.system ("cls")
+
+while i<=len(Com_Votantes)-1:
+     print("Votantes{I} : ",Com_Votantes[i])
+     if i!=2+j and i!=3+j and i<=4+j:
+         Votante.append(Com_Votantes[i+j])
+     if i==2+j:
+         Votante.append(Com_Votantes[2+j]+" "+Com_Votantes[3+j])
+     if i==5: 
+           j+=6;
+     i+=1
 
  
- 
-print("->",Votantes) 
+print("->",Votante)
 #print("----",linea) 
 V1.close()
    
