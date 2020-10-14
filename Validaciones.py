0db88fc208cebb7119987364e5d4ce1de9faa55c
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2020-10-14 02:23:09 -0500

    Version_1.0

--- Validaciones.py
+++ Validaciones.py
@@ -4,29 +4,28 @@ import os
 import sys
 
 
+V1 = ""; V2 = "";k=0;Votantes=[];
 
-V1 = "";V2=""; Votantes=[] ; Datos = "" ;i=0;j=0;Com=""
 V1    = open("Prueba.txt","r") 
 V2 = V1.readlines()
 V1.close() 
    
 
-
-Datos = V2[0]
-
-while i<=2:
-  while Datos[j]!="-":
-      Com = Com + Datos[j]
-      j+=1
-  Votantes.append(Com)
-  Com=""  
-  i+=1
-  j+=1
-if len(Votantes)==3:
-      Com = ""
-      Com = Datos[j:Datos.find("*")]
-      Votantes.append(Com)
-      #print("DATOS : ",Com)
-      
+while k<=len(V2):
+    Datos=""; Com =""; i=0 ;j=0
+    Datos = V2[0]
+    while i<=2:
+       while Datos[j]!="-":
+          Com = Com + Datos[j]
+          j+=1
+       Com = Com.replace(" ","")
+       Votantes.append(Com)
+       Com="";i+=1;j+=1
+    if len(Votantes)%3==0:
+       Com = ""
+       Com = Datos[j:Datos.find("*")]
+       Com = Com.replace(" ","")
+       Votantes.append(Com)
+    k+=1  
  
 print("Actualizacion : ",Votantes)  
